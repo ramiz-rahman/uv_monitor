3b3bff8c2906ae9e388b4393a3cd6e810961ac8a
ramiz-rahman <ramiz.rahman@outlook.com>
2020-02-07 17:32:44 +0600

    feat: Create the home screen

---
+++ lib/home_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import 'package:uv_monitor/home_widgets/top_half.dart';
+import 'package:uv_monitor/home_widgets/bottom_half.dart';
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final HomeBody = Container(
+      constraints: BoxConstraints.expand(),
+      color: Color(0xFF371502),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          TopHalf(),
+          BottomHalf(),
+        ],
+      ),
+    );
+
+    return Scaffold(
+      body: SafeArea(
+        child: HomeBody,
+      ),
+    );
+  }
+}
