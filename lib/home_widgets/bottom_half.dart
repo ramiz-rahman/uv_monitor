218b42bc30fb68c92bd0e4af20537e9cabdbfdeb
ramiz-rahman <ramiz.rahman@outlook.com>
2020-02-07 17:32:16 +0600

    feat: Create bottom part of the homescreen

---
+++ lib/home_widgets/bottom_half.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import 'package:uv_monitor/home_widgets/button.dart';
+import 'package:uv_monitor/home_widgets/geolocation.dart';
+
+class BottomHalf extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            _Buttons(),
+            Geolocation(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Buttons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(30.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Button(
+            text: 'Refresh',
+            color: Color(0xFF2B8700),
+          ),
+          Button(
+            text: 'Logout',
+            color: Color(0xFF870008),
+          ),
+        ],
+      ),
+    );
+  }
+}
