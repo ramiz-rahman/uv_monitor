de61de73830c5752a52905fdad235dd7ab312e73
ramiz-rahman <ramiz.rahman@outlook.com>
2020-02-10 12:51:13 +0600

    feat: UVIndex widget should show date and time

--- lib/home_widgets/uv_index.dart
+++ lib/home_widgets/uv_index.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 class UVIndex extends StatelessWidget {
+  double index;
+  String datetime;
+  UVIndex({this.index, this.datetime});
+
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: <Widget>[
-          _Index('5.9678'),
+          _Index(index: this.index),
           _Label(),
+          _Datetime(datetime: this.datetime),
         ],
       ),
     );
@@ -15,13 +20,13 @@ class UVIndex extends StatelessWidget {
 }
 
 class _Index extends StatelessWidget {
-  String index;
-  _Index(this.index);
+  double index;
+  _Index({this.index});
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      index,
+      "${this.index}",
       textAlign: TextAlign.center,
       style: TextStyle(
         fontSize: 64,
@@ -44,6 +49,28 @@ class _Label extends StatelessWidget {
         height: 1.5,
         fontWeight: FontWeight.w500,
         color: Color(0xFF676767),
+      ),
+    );
+  }
+}
+
+class _Datetime extends StatelessWidget {
+  String datetime;
+
+  _Datetime({this.datetime});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 40),
+      child: Text(
+        datetime,
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 18,
+          height: 1.5,
+          color: Color(0xFF676767),
+        ),
       ),
     );
   }
